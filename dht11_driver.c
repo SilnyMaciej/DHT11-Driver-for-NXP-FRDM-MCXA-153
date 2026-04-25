ea594f727f2a66efcae83a4c1090fb677724a5a3
Maciej Jan <maciej.mosio@op.pl>
2026-04-25 09:43:40 +0200

    Refactor DHT11 GPIO pin definitions for clarity

--- dht11_driver.c
+++ dht11_driver.c
@@ -4,44 +4,47 @@
 #include "pin_mux.h"
 #include "delay_us.h"
 
+#define DHT11_GPIO DHT11_INITPINS_DHT11_GPIO
+#define DHT11_PIN_MASK DHT11_INITPINS_DHT11_GPIO_PIN_MASK
+
 int8_t DHT11_Get_Temperature_And_RH(uint8_t *temp_int,uint8_t *temp_dec,uint8_t *rh_int,uint8_t *rh_dec){
     uint8_t data[5] = {0};
 
-    DHT11_INITPINS_DHT11_GPIO->PDDR |= DHT11_INITPINS_DHT11_GPIO_PIN_MASK;
+    DHT11_GPIO->PDDR |= DHT11_PIN_MASK;
 
-    DHT11_INITPINS_DHT11_GPIO->PCOR = DHT11_INITPINS_DHT11_GPIO_PIN_MASK;
+    DHT11_GPIO->PCOR = DHT11_PIN_MASK;
     delay_us(18000U);
 
-    DHT11_INITPINS_DHT11_GPIO->PSOR =  DHT11_INITPINS_DHT11_GPIO_PIN_MASK;
+    DHT11_GPIO->PSOR = DHT11_PIN_MASK;
     delay_us(30U);
 
-    DHT11_INITPINS_DHT11_GPIO->PDDR &= ~ DHT11_INITPINS_DHT11_GPIO_PIN_MASK;
+    DHT11_GPIO->PDDR &= ~DHT11_PIN_MASK;
 
-    while(DHT11_INITPINS_DHT11_GPIO->PDIR &  DHT11_INITPINS_DHT11_GPIO_PIN_MASK){
+    while(DHT11_GPIO->PDIR & DHT11_PIN_MASK){
     	__asm("nop");
     }
 
-    while(!(DHT11_INITPINS_DHT11_GPIO->PDIR &  DHT11_INITPINS_DHT11_GPIO_PIN_MASK)){
+    while(!(DHT11_GPIO->PDIR & DHT11_PIN_MASK)){
     	__asm("nop");
     }
 
-    while(DHT11_INITPINS_DHT11_GPIO->PDIR &  DHT11_INITPINS_DHT11_GPIO_PIN_MASK){
+    while(DHT11_GPIO->PDIR & DHT11_PIN_MASK){
        	__asm("nop");
 
     }
 
     for(uint8_t i = 0; i < 40; i++){
 
-        while(!(DHT11_INITPINS_DHT11_GPIO->PDIR &  DHT11_INITPINS_DHT11_GPIO_PIN_MASK)){
+        while(!(DHT11_GPIO->PDIR & DHT11_PIN_MASK)){
         	__asm("nop");
         }
 
         delay_us(35U);
 
-        if(DHT11_INITPINS_DHT11_GPIO->PDIR &  DHT11_INITPINS_DHT11_GPIO_PIN_MASK){
+        if(DHT11_GPIO->PDIR & DHT11_PIN_MASK){
             data[i / 8] |= (1U << (7U - (i % 8U)));
 
-            while(DHT11_INITPINS_DHT11_GPIO->PDIR &  DHT11_INITPINS_DHT11_GPIO_PIN_MASK){
+            while(DHT11_GPIO->PDIR & DHT11_PIN_MASK){
             	__asm("nop");
             }
         }
