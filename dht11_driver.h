ea5c8a45c890332e84cf717756f5b252c244b42d
Maciej Jan <maciej.mosio@op.pl>
2026-05-06 09:04:19 +0200

    Add GPIO definitions for DHT11 driver

--- dht11_driver.h
+++ dht11_driver.h
@@ -8,6 +8,10 @@
 #ifndef DHT11_DRIVER_H_
 #define DHT11_DRIVER_H_
 
+#include "delay_us.h"
+
+#define DHT11_GPIO DHT11_INITPINS_DHT11_GPIO
+#define DHT11_PIN_MASK DHT11_INITPINS_DHT11_GPIO_PIN_MASK
 
 uint32_t DHT11_Get_Temperature_And_RH(void);
 #endif /* DHT11_DRIVER_H_ */
