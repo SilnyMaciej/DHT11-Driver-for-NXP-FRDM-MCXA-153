4d6c251e33a6220b6ef131af93f8da028a5f3302
Maciej Jan <maciej.mosio@op.pl>
2026-05-07 22:54:24 +0200

    Refactor DHT11_Get_Temperature_And_RH function

--- dht11_driver.c
+++ dht11_driver.c
@@ -4,7 +4,8 @@
 
 uint32_t DHT11_Get_Temperature_And_RH(void){
 
-    uint32_t data = 0;
+	volatile uint32_t timeout = DHT11_TIMEOUT_CYCLES;
+    uint8_t data[5] = {0};
 
     DHT11_GPIO->PCOR = DHT11_PIN_MASK;
     DHT11_GPIO->PDDR |= DHT11_PIN_MASK;
@@ -13,43 +14,54 @@ uint32_t DHT11_Get_Temperature_And_RH(void){
     delay_us(18000U);
 
     DHT11_GPIO->PDDR &= ~DHT11_PIN_MASK;
-    delay_us(30U);
 
-
-    while(DHT11_GPIO->PDIR & DHT11_PIN_MASK){
-    	__asm("nop");
-    }
+    delay_us(42U);
 
     while(!(DHT11_GPIO->PDIR & DHT11_PIN_MASK)){
     	__asm("nop");
+    	 if(--timeout == 0) return GND_ERROR;
     }
+
+
+    timeout = DHT11_TIMEOUT_CYCLES;
 
     while(DHT11_GPIO->PDIR & DHT11_PIN_MASK){
-       	__asm("nop");
+    	__asm("nop");
+
+    	if(--timeout == 0) return VCC_ERROR;
 
     }
+
+    uint8_t bit = 0;
 
     for(uint8_t i = 0; i < 40; i++){
 
-        while(!(DHT11_GPIO->PDIR & DHT11_PIN_MASK)){
-        	__asm("nop");
-        }
+    	timeout = DHT11_TIMEOUT_CYCLES;
+
+    	while(!(DHT11_GPIO->PDIR & DHT11_PIN_MASK)){
+    		__asm("nop");
+
+    		if(--timeout == 0) return GND_ERROR;
+    	 }
 
         delay_us(35U);
 
-        if(DHT11_GPIO->PDIR & DHT11_PIN_MASK){
+        bit = (DHT11_GPIO->PDIR & DHT11_PIN_MASK) != 0;
+        *(data + (i >> 3)) |= (bit << (7 - (i & 7)));
 
-        	if (i < 8) 	 data |= (1U << (31 - i));
+        while(DHT11_GPIO->PDIR & DHT11_PIN_MASK){
+        	__asm("nop");
 
-        	else if (i >= 16)  data |= (1U << (39 - i));
-
-
-        	while(DHT11_GPIO->PDIR & DHT11_PIN_MASK){
-        		__asm("nop");
-        	}
+        	if(--timeout == 0) return VCC_ERROR;
         }
-    }
 
-    return (((uint8_t)(data >> 24) + (uint8_t)(data >> 16)) + (uint8_t)((data >> 8)) == (uint8_t)(data & 0xFF)) ? data : 200;
+
+    }
+    
+   if(*data + *(data + 1) + *(data + 2) + *(data + 3) == *(data + 4)){
+
+	  return ((uint32_t)(*(data)) << 24) | ((uint32_t)(*(data + 1)) << 16) | ((uint32_t)(*(data + 2)) << 8) | ((uint32_t)(*(data + 3)));
+   }
+   return CHECK_SUM_ERROR;
 
 }
